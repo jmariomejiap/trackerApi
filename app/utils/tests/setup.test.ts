d8354c66cbfd57eb05a05ba8ad3b5cc2ab8d0fcc
jmario mejia <jmariomejiap@gmail.com>
2020-01-21 00:31:25 -0400

    find test bug 2

--- app/utils/tests/setup.test.ts
+++ app/utils/tests/setup.test.ts
@@ -6,10 +6,10 @@ before(async () => {
   console.log("MONGODB_URI_LOCAL", process.env.MONGODB_URI_LOCAL);
   mongoose.connect("mongodb://localhost:27017/tracker-api-test", {
     // mongoose.connect(String(process.env.MONGODB_URI_LOCAL), {
-    // bufferCommands: false,
+    bufferCommands: false,
     keepAlive: true,
     keepAliveInitialDelay: 300000,
-    // useCreateIndex: true,
+    useCreateIndex: true,
     useNewUrlParser: true
   });
 
